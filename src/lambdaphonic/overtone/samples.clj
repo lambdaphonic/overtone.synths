ff4cc7cecedb275f6756efc97d4badbb334aabca
Martin Gondermann <magicmonty@pagansoft.de>
2016-04-09 11:10:39 +0200

    Add some nice Kick samples

---
+++ src/lambdaphonic/overtone/samples.clj
@@ -0,0 +1,11 @@
+(ns lambdaphonic.overtone.samples
+  (:use [overtone.live :refer :all]))
+
+
+(defonce deep-kick (freesound 264285))
+(defonce big-reverb-kick (freesound 219514))
+(defonce big-reverb-kick2 (freesound 219515))
+(defonce fat-kick (freesound 248142))
+(defonce mingo-kick (freesound 213934))
+
+
